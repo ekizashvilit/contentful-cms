bc6c46f6186b98180214de03e13b557109f6081b
Teona <tekizashvili@gmail.com>
2023-05-07 11:40:59 +0400

    projects data

---
+++ src/data.js
@@ -0,0 +1,22 @@
+const projects = [
+  {
+    title: 'stock photos',
+    url: 'https://stock-photos-ekizashvilit.netlify.app/',
+    image: './assets/stock-photos.jpg',
+  },
+  {
+    title: 'news website',
+    url: 'https://news-website-ekizashvilit.netlify.app/',
+    image: './assets/news-website.jpg',
+  },
+  {
+    title: 'search github users',
+    url: 'https://github-users-search-ekizashvilit.netlify.app/login',
+    image: './assets/search-github-users.jpg',
+  },
+  {
+    title: 'quiz',
+    url: 'https://quiz-ekizashvilit.netlify.app/',
+    image: './assets/quiz.jpg',
+  },
+];
